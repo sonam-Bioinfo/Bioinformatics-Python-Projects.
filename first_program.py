2df415cf1e570d189ac4065444efb02c7c8f92d6
sonam-Bioinfo <sonam.studies.uk@gmail.com>
2026-02-19 08:03:47 +0530

    Create first_program.py

---
+++ first_program.py
@@ -0,0 +1,11 @@
+# ----DNA sequence Analysis----
+# 1. DNA sequence ko variable mein save karna
+dna_sequence = "ATGCATGC"
+# 2. Sequence ko screen per dikhana 
+print("Mera DNA sequence hai:", dna_sequence)
+# 3. Sequence ki lambai (length) ginna
+lambai = len(dna_sequence)
+print("Is DNA ki lambai hai:", lambai)
+# 4. Kisi khaas Base (Nucleotide) ko count karna
+a_count = dna_sequence.count("A")
+print("Isme total Akitne hain:", a_count)
