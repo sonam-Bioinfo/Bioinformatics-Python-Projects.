140fb6c5b903af8775cf6334777350e9d44f7abc
sonam-Bioinfo <sonam.studies.uk@gmail.com>
2026-02-19 20:45:33 +0530

    Implement DNA to RNA transcription functionality
    Add a program for DNA to RNA transcription.

---
+++ second_program.py
@@ -0,0 +1,8 @@
+# ----DNA to RNA Transcription----
+# 1. Hamara DNA sequence 
+dna = "ATGCATGCATGC"
+print("DNA:", dna)
+# 2. Transcription: T ko U se badalna
+# .replace() function kisi bhi letter ko badalne ke kaam aata hai
+rna = dna.replace("T", "U")
+print("RNA:", rna)
